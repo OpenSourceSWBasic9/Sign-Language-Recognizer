4a267230f6df36dfb39d0027f47dbf23863fa262
1000Pumpkin <chunho5012@gmail.com>
2026-06-09 07:57:53 +0900

    새 모델 적용, 입력 대기 3초 후 단어 리스트 초기화

--- keypoint.py
+++ keypoint.py
@@ -7,8 +7,8 @@ import torch.nn as nn
 from collections import deque
 from PIL import ImageFont, ImageDraw, Image
 
-feature_dim = 132
-MAX_FRAME = 123
+feature_dim = 135
+MAX_FRAME = 175
 THRESHOLD = 0.85
 
 frame_buffer = deque([[0.0] * feature_dim] * MAX_FRAME, maxlen=MAX_FRAME)
@@ -16,6 +16,9 @@ word_sequence_queue = []
 
 hand_visible_counter = 0
 MIN_REQUIRED_FRAMES = 30
+
+prev_time = 0.0
+waiting_time = 0.0
 
 # 한글 폰트
 try:
@@ -40,13 +43,13 @@ pose = mp_pose.Pose(
 
 # 모델 파일 로드
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-save_path = "sign_language_model_v2.pth"
+save_path = "sign_language_model_v3.pth"
 checkpoint = torch.load(save_path, map_location=device)
 
 # 정보 추출
 loaded_word_to_idx = checkpoint['word_to_idx']
 idx_to_word = {v: k for k, v in loaded_word_to_idx.items()}
-hidden_size = checkpoint['hidden_size']
+hidden_size = checkpoint['hidden_dim']
 num_layers = checkpoint['num_layers']
 output_dim = len(loaded_word_to_idx)
 
@@ -54,9 +57,9 @@ output_dim = len(loaded_word_to_idx)
 class SignLanguageClassifier(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
         super(SignLanguageClassifier, self).__init__()
-        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True, dropout= 0.3, bidirectional=True)
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True, dropout= 0.5, bidirectional=True)
         self.fc = nn.Sequential(
-            nn.Linear(hidden_dim * 2, hidden_dim),
+            nn.Linear(hidden_dim * 4, hidden_dim),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(hidden_dim, output_dim)
@@ -67,7 +70,7 @@ class SignLanguageClassifier(nn.Module):
         avg_pool = torch.mean(out, dim=1)
         max_pool, _ = torch.max(out, dim=1)
 
-        combined = avg_pool + max_pool
+        combined = torch.cat((avg_pool, max_pool), dim=1)
 
         return self.fc(combined)
 
@@ -75,12 +78,14 @@ model = SignLanguageClassifier(feature_dim, hidden_size, output_dim, num_layers)
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
+# 웹캠 캡처
 cap = cv2.VideoCapture(0)
 
 if cap.isOpened():
     print("카메라가 성공적으로 켜졌습니다.", flush=True)
     print("종료하려면 카메라 창 클릭 후 q를 누르세요.", flush=True)
     time.sleep(2)
+    prev_time = time.time()
 else:
     print("카메라 연결 실패")
 
@@ -129,7 +134,7 @@ while cap.isOpened():
 
     frame_keypoints = frame_keypoints[:126]
 
-    #코, 양쪽 어깨 좌표 추출
+    # 코, 양쪽 어깨 좌표 추출
     extra_features = []
     
     if results_pose.pose_landmarks:
@@ -138,21 +143,19 @@ while cap.isOpened():
 
         left_shoulder = results_pose.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
         right_shoulder = results_pose.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-        
-        shoulder_center_x = (left_shoulder.x + right_shoulder.x) / 2
-        shoulder_center_y = (left_shoulder.y + right_shoulder.y) / 2
 
-        extra_features.extend([shoulder_center_x, shoulder_center_y, 1])
+        extra_features.extend([right_shoulder.x, right_shoulder.y, 1, left_shoulder.x, left_shoulder.y, 1])
 
         h, w, c = frame.shape
         cv2.circle(frame, (int(nose.x * w), int(nose.y * h)), 6, (0, 255, 0), cv2.FILLED)
         cv2.circle(frame, (int(left_shoulder.x * w), int(left_shoulder.y * h)), 8, (0, 0, 255), cv2.FILLED)
         cv2.circle(frame, (int(right_shoulder.x * w), int(right_shoulder.y * h)), 8, (0, 0, 255), cv2.FILLED)
     else:
-        extra_features = [0, 0, 1, 0, 0, 1]
+        extra_features = [0, 0, 1, 0, 0, 1, 0, 0, 1]
 
     frame_keypoints = frame_keypoints + extra_features
 
+    # 코 좌표 기준 상대 좌표 계산
     ref_x, ref_y = extra_features[0], extra_features[1]
 
     x_indices = list(range(0, feature_dim, 3))
@@ -162,11 +165,17 @@ while cap.isOpened():
     frame_keypoints[y_indices] = (frame_keypoints[y_indices] - ref_y)
     frame_keypoints = frame_keypoints.tolist()
 
+    cur_time = time.time()
+    delta_time = cur_time - prev_time
     is_hand_detected = results_hands.multi_hand_landmarks is not None
 
     if not is_hand_detected:
         frame_buffer = deque([[0.0] * feature_dim] * MAX_FRAME, maxlen=MAX_FRAME)
+        waiting_time += delta_time
+        if waiting_time >= 3.0:
+            word_sequence_queue = []
     else:
+        waiting_time = 0.0
         hand_visible_counter += 1
 
         frame_buffer.append(frame_keypoints)
@@ -198,6 +207,7 @@ while cap.isOpened():
         draw.text((30, 50), sentence_text, font=font, fill=(255, 0, 0))
         frame = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
 
+    prev_time = cur_time
     cv2.imshow("Su-eo Project: Hand & Pose Tracking", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
